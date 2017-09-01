e54e1a60003fc5d5ff594be0375f877809f16307
Wang HongYin <wanghongyin@shinetechchina.com>
2017-09-01 16:39:37 +0800

    Init.

---
+++ UnitTestDemo.Tests/common/CmpTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDemo.common;
+
+namespace UnitTestDemo.Tests.common
+{
+    [TestClass]
+    public class CmpTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestLargest()
+        {
+            // 数组中元素从小到大排列
+            Assert.AreEqual(9, Cmp.Largest(new[] { 1, 3, 9 }));
+
+            // 数组中元素从大到小排列
+            Assert.AreEqual(9, Cmp.Largest(new[] { 9, 3, 1 }));
+
+            // 包含最大值和最小值
+            Assert.AreEqual(int.MaxValue, Cmp.Largest(new[] { int.MinValue, int.MaxValue, 0 }));
+
+            // 包含负数和0
+            Assert.AreEqual(1, Cmp.Largest(new[] { -1, 1, 0 }));
+
+            // 数组中只有一个元素
+            Assert.AreEqual(0, Cmp.Largest(new[] { 0 }));
+        }
+
+        [TestMethod]
+        public void TestLargestWithAllNegativeData()
+        {
+            Assert.AreEqual(-1, Cmp.Largest(new[] { int.MinValue, -10, -1 }));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestLargestWithEmptyList()
+        {
+            Cmp.Largest(new int[] { });
+        }
+
+        [TestMethod]
+        [TestCategory("short")]
+        public void OtherTest()
+        {
+            Assert.AreEqual(3.33, 10.0 / 3, 0.01);
+
+            Assert.IsNull(null);
+            Assert.IsFalse(1 + 1 > 2);
+            Assert.AreSame("sss", "sss");
+        }
+    }
+}
---
+++ UnitTestDemo/common/Cmp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace UnitTestDemo.common
+{
+    public class Cmp
+    {
+        //public static int Largest(int[] list)
+        //{
+        //    int max = int.MinValue;
+        //    if (list.Length == 0)
+        //    {
+        //        throw new ArgumentException("Largest: list cannot be empty");
+        //    }
+
+        //    foreach (int item in list) // we use foreach to avoid "off-by-one" error
+        //    {
+        //        if (item > max)
+        //        {
+        //            max = item;
+        //        }
+        //    }
+        //    return max;
+        //}
+
+        //public static int Largest(int[] list)
+        //{
+        //    int max = int.MinValue;
+        //    if (list.Length == 0)
+        //    {
+        //        throw new ArgumentException("Largest: list cannot be empty");
+        //    }
+
+        //    for (int i = 0; i < list.Length -1; i++) // error: for loop ends too early(off-by-one)
+        //    {
+        //        if (list[i] > max)
+        //        {
+        //            max = list[i];
+        //        }
+        //    }
+
+        //    return max;
+        //}
+
+        public static int Largest(int[] list)
+        {
+            int max = 0; // error: we want the max with min value as its initial value
+            if (list.Length == 0)
+            {
+                throw new ArgumentException("Largest: list cannot be empty");
+            }
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
